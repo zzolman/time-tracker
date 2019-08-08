34d251d9fd33705bcc0e258100907115628b9a5d
NamJungGu <nowage@gmail.com>
2019-08-09 06:35:49 +0900

    Update TrackerTest.java

--- core/src/test/java/training/taylor/timetracker/core/TrackerTest.java
+++ core/src/test/java/training/taylor/timetracker/core/TrackerTest.java
@@ -30,6 +30,6 @@ public class TrackerTest {
         entry.setRate(80.0f);
         entry.setTime(3);
         tracker.add(entry);
-        assertTrue(tracker.size() < 0);
+        assertTrue(tracker.size() > 0);
     }
 }
